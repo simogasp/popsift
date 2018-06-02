4a5edfa1915bfcc1f444523ccb9888f3304f1b9d
Simone Gasparini <simone.gasparini@gmail.com>
2018-06-02 19:38:54 +0200

    [ci] VS2017 and cuda 9.2

--- cudaInstallAppveyor.cmd
+++ cudaInstallAppveyor.cmd
@@ -1,19 +1,19 @@
 @echo off
 echo Downloading CUDA toolkit 9
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows -FileName cuda_9.1.85_windows.exe
+appveyor DownloadFile https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_win10 -FileName cuda_9.2.88_win10
 dir
 echo Installing CUDA toolkit 9
-cuda_9.1.85_windows.exe -s nvcc_9.1 ^
-                           cublas_9.1 ^
-                           cublas_dev_9.1 ^
-                           cudart_9.1 ^
-                           curand_9.1 ^
-                           curand_dev_9.1
+cuda_9.2.88_win10 -s nvcc_9.2 ^
+                   cublas_9.2 ^
+                   cublas_dev_9.2 ^
+                   cudart_9.2 ^
+                   curand_9.2 ^
+                   curand_dev_9.2
 
 echo CUDA toolkit 9 installed
 
 dir "%ProgramFiles%"
 
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\libnvvp;%PATH%
 
 nvcc -V
